842419e6b279b2b28151d1dce9fc0a65edf4ca85
вікторія050 <vikapiddubna0508@gmail.com>
2024-12-20 21:47:22 +0200

    print

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-class Car:
-    def __init__(self, make, model, year):
-        self.make = make  
-        self.model = model  
-        self.year = year  
-
-    def get_info(self):
-        return f"{self.year} {self.make} {self.model}"  
-
-
-my_car = Car("BMW", "X5", 2023 )
-print(my_car.get_info())  
+print("Hello")  
